28e2e1463c8650ac9f3caec7aa1793b2fc89dd9d
Saicharan Gouru <gourusaicharan6@gmail.com>
2023-12-12 12:11:04 +0530

    environment variables setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,20 +7,24 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+console.log(process.env)
 const firebaseConfig = {
-    apiKey: "AIzaSyCnGZ637gP5DK009R2yhmTTdFaJ-X5fwBA",
-    authDomain: "netflix-gpt-b1136.firebaseapp.com",
-    projectId: "netflix-gpt-b1136",
-    storageBucket: "netflix-gpt-b1136.appspot.com",
-    messagingSenderId: "145219840431",
-    appId: "1:145219840431:web:773da64488e51d9573275a",
-    measurementId: "G-1LDKVKCT6C"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth();
+const auth = getAuth();
 
-console.log(analytics)
+console.log(analytics);
+
+export { auth }
