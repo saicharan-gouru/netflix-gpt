83a776a24658b1de13dff2fcd9c533997e19f2e3
Saicharan Gouru <gourusaicharan6@gmail.com>
2023-12-19 17:56:07 +0530

    Bug fix

--- src/components/Browse/MainContainer.js
+++ src/components/Browse/MainContainer.js
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux"
-import { useMainMovieTrailer } from "../../hooks";
+import VideoBackground from "./VideoBackground";
+
 
 
 
 
 const MainContainer = () => {
     const movies = useSelector(store => store.movies?.nowPlaying);
+    if (!movies) return;
     const mainMovie = movies[0]
-    useMainMovieTrailer(mainMovie)
-    const trailer = useSelector(store => store.movies?.mainMovieTrailer)
+
+
 
   return (
     <div>
@@ -16,14 +18,7 @@ const MainContainer = () => {
             <h1 className="text-3xl font-bold">{mainMovie.title}</h1>
             <p className="w-1/4 py-3">{mainMovie.overview}</p>
         </div>
-        <div className="w-screen">
-        <iframe 
-        className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+trailer?.key+"?&autoplay=1&mute=1&loop=1" }
-        frameborder="0"
-        title="YouTube video player"  
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-        </div>
+        <VideoBackground mainMovie={mainMovie}/>
     </div>
   )
 }
